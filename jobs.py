7d9264cf364ee25fb1a4abfa99ed21144f30e8ca
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-02-07 17:42:34 +0300

    Can run jobs

--- jobs.py
+++ jobs.py
@@ -7,6 +7,11 @@ from sklearn.model_selection import train_test_split
 from loaders import load_batch_of_images, load_model_pb
 from utils.metrics import cosine_distance
 from siamese import train_siamese_model
+import argparse
+
+import sys
+sys.path.append("..")
+from input.models.deep_sort_cnn.freeze_model import create_graph
 
 def siamese_job(source_path, model_path, **kwargs):
 
@@ -20,7 +25,7 @@ def siamese_job(source_path, model_path, **kwargs):
     )(source_path)
 
     labels = classes_to_labels(dirs)
-    train_dirs, val_dirs, train_labels, val_labels = train_test_split(dirs[0:20], labels[0:20], test_size=0.1)
+    train_dirs, val_dirs, train_labels, val_labels = train_test_split(dirs, labels, test_size=0.1)
 
     inputs, outputs, _ = load_model_pb(
         model_path, 
@@ -43,4 +48,36 @@ def siamese_job(source_path, model_path, **kwargs):
         num_iter=2,
     )
 
-    
+def parse_args():
+    """Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Freeze old model")
+    parser.add_argument(
+        "--job_name",
+        required=True,
+        choices=['siamese']
+    )
+    parser.add_argument(
+        "--source_path",
+        required=True,
+        help="Path to the data"
+    )
+    parser.add_argument(
+        "--model_path",
+        required=True,
+        help="Path to the model"
+    )
+    parser.add_argument(
+        "--use_graph_creator",
+        default=False,
+    )
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    if args.job_name == 'siamese':
+        siamese_job(args.source_path, args.model_path, graph_creator=create_graph if args.use_graph_creator else None)
+
+if __name__ == '__main__':
+    main()
