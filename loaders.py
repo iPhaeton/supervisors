4020a3690aab6d6ee416071b3a7a1f4763d1f412
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-02-08 01:10:12 +0300

    Define load_simple_model

--- loaders.py
+++ loaders.py
@@ -130,3 +130,18 @@ def load_model_pb(checkpoint_filename, input_name, output_name, **kwargs):
         outputs = tf.get_default_graph().get_tensor_by_name("net/%s:0" % output_name)
     
     return inputs, outputs, graph_def
+
+def load_simple_model():
+    X = tf.placeholder(tf.float32, [None, 32, 32, 3])
+    # setup variables
+    Wconv1 = tf.get_variable("Wconv1", shape=[7, 7, 3, 32])
+    bconv1 = tf.get_variable("bconv1", shape=[32])
+    W1 = tf.get_variable("W1", shape=[5408, 10])
+    b1 = tf.get_variable("b1", shape=[10])
+
+    # define our graph (e.g. two_layer_convnet)
+    a1 = tf.nn.conv2d(X, Wconv1, strides=[1,2,2,1], padding='VALID') + bconv1
+    h1 = tf.nn.relu(a1)
+    h1_flat = tf.reshape(h1,[-1,5408])
+    y_out = tf.matmul(h1_flat,W1) + b1
+    return X, y_out
