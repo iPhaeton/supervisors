1bd58e92569b85e9c2c95eca072ff1b2b4c2c8f4
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-02-08 17:14:59 +0300

    Update simple model

--- loaders.py
+++ loaders.py
@@ -132,15 +132,26 @@ def load_model_pb(checkpoint_filename, input_name, output_name, **kwargs):
     return inputs, outputs, graph_def
 
 def load_simple_model():
-    X = tf.placeholder(tf.float32, [None, 32, 32, 3])
+    X = tf.placeholder(tf.float32, [None, 64, 64, 3])
     # setup variables
-    Wconv1 = tf.get_variable("Wconv1", shape=[7, 7, 3, 32])
+    Wconv0 = tf.get_variable("Wconv0", shape=[2, 2, 3, 16])
+    bconv0 = tf.get_variable("bconv0", shape=[16])
+    Wconv1 = tf.get_variable("Wconv1", shape=[7, 7, 16, 32])
     bconv1 = tf.get_variable("bconv1", shape=[32])
     W1 = tf.get_variable("W1", shape=[5408, 10])
     b1 = tf.get_variable("b1", shape=[10])
 
+    tf.summary.histogram('Wconv0', Wconv0)
+    tf.summary.histogram('bconv0', bconv0)
+    tf.summary.histogram('Wconv1', Wconv1)
+    tf.summary.histogram('bconv1', bconv1)
+    tf.summary.histogram('W1', W1)
+    tf.summary.histogram('b1', b1)
+
     # define our graph (e.g. two_layer_convnet)
-    a1 = tf.nn.conv2d(X, Wconv1, strides=[1,2,2,1], padding='VALID') + bconv1
+    a0 = tf.nn.conv2d(X, Wconv0, strides=[1,2,2,1], padding='VALID') + bconv0
+    h0 = tf.nn.relu(a0)
+    a1 = tf.nn.conv2d(h0, Wconv1, strides=[1,2,2,1], padding='VALID') + bconv1
     h1 = tf.nn.relu(a1)
     h1_flat = tf.reshape(h1,[-1,5408])
     y_out = tf.matmul(h1_flat,W1) + b1
