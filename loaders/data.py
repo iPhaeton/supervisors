8595c313b5a4620af60d0c4d1bd8a6604d816213
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-02-10 08:35:56 +0300

    Define cifar10 loaders

--- loaders/data.py
+++ loaders/data.py
@@ -5,6 +5,8 @@ import os
 from pyramda import curry
 from decorators import partially_applied
 import cv2
+import platform
+from six.moves import cPickle as pickle
 
 #loads images in RGB format
 def pil_loader(path, image_shape):
@@ -81,3 +83,64 @@ def load_batch_of_images(path, path_dirs, labels, num_per_class, batch_size=None
         batch_labels[i*num_per_class: i*num_per_class + num_per_class] = batch_labels[i*num_per_class: i*num_per_class + num_per_class] * labels[i]
     
     return samples, batch_labels
+
+def load_pickle(f):
+    version = platform.python_version_tuple()
+    if version[0] == '2':
+        return  pickle.load(f)
+    elif version[0] == '3':
+        return  pickle.load(f, encoding='latin1')
+    raise ValueError("invalid python version: {}".format(version))
+
+def load_CIFAR_batch(filename):
+    """ load single batch of cifar """
+    with open(filename, 'rb') as f:
+        datadict = load_pickle(f)
+        X = datadict['data']
+        Y = datadict['labels']
+        X = X.reshape(10000, 3, 32, 32).transpose(0,2,3,1).astype("float")
+        Y = np.array(Y)
+        return X, Y
+
+def load_CIFAR10(path):
+    """ load all of cifar """
+    xs = []
+    ys = []
+    for b in range(1,4):
+        f = os.path.join(path, 'data_batch_%d' % (b, ))
+        X, Y = load_CIFAR_batch(f)
+        xs.append(X)
+        ys.append(Y)
+    Xtr = np.concatenate(xs)
+    Ytr = np.concatenate(ys)
+    del X, Y
+    Xte, Yte = load_CIFAR_batch(os.path.join(path, 'test_batch'))
+    return Xtr, Ytr, Xte, Yte
+
+def load_CIFAR10_data(path, num_training=29000, num_validation=1000, num_test=10000):
+    # Original code: 
+    """
+    Load the CIFAR-10 dataset from disk and perform preprocessing.  
+    """
+    # Load the raw CIFAR-10 data
+    cifar10_dir = path
+    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+    # Subsample the data
+    mask = range(num_training, num_training + num_validation)
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+    mask = range(num_training)
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = range(num_test)
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    # Normalize the data: subtract the mean image
+    mean_image = np.mean(X_train, axis=0)
+    X_train -= mean_image
+    X_val -= mean_image
+    X_test -= mean_image
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
