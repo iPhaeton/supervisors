c70d44e1a5c6b05bd16d7d6c8f02516cc02528f4
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-02-11 15:01:27 +0300

    Run siamese job in run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,20 @@
-# python jobs.py \
-#     --job_name=siamese \
-#     --source_path=../input/mars/bbox_train/ \
-#     --model_path=../input/models/deep_sort_cnn/mars-small128.pb \
-#     --use_graph_creator=True \
-#     --batch_size=30 \
-#     --num_iter=100 \
-#     --num_per_class=4 \
-#     --margin=0.2 \
-#     --lr=1e-4
-
 python jobs.py \
-    --job_name=classifier \
-    --source_path=../input/cifar-10-batches-py \
-    --model_name=complex \
-    --loss=softmax \
-    --data=cifar10 \
-    --batch_size=64 \
-    --num_iter=1 \
+    --job_name=siamese \
+    --source_path=../input/mars/bbox_train/ \
+    --model_path=../input/models/deep_sort_cnn/mars-small128.pb \
+    --use_graph_creator=True \
+    --batch_size=30 \
+    --num_iter=100 \
+    --num_per_class=4 \
+    --margin=0.2 \
     --lr=1e-4
+
+# python jobs.py \
+#     --job_name=classifier \
+#     --source_path=../input/cifar-10-batches-py \
+#     --model_name=complex \
+#     --loss=softmax \
+#     --data=cifar10 \
+#     --batch_size=64 \
+#     --num_iter=1 \
+#     --lr=1e-4
