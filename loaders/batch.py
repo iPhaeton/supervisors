8bb012917e250cef4cca0e7b46c5c75bf498ee99
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-02-15 14:16:47 +0300

    Define load_batch_of_images

--- loaders/batch.py
+++ loaders/batch.py
@@ -17,11 +17,83 @@ def cv2_loader(path, image_shape):
     img = cv2.resize(img, (image_shape[1], image_shape[0]))
     return np.array(img)
 
-def batch_of_images_generator(path, dirs, labels, image_shape, loader, num_per_class, batch_size=None, random=False):
+def load_batch_of_images(path, dirs, labels, image_shape, loader, num_per_class, batch_size=None, random=False, start_idx=None, end_idx=None):
     """
     Loads a random batch of images
     
-    Parameters:
+    Keyword Arguments:
+    -----------
+    - path: string
+        Path to the image source directory on disk. 
+        Source directory should be divided into directories, one directory per class.
+    - dirs: [string]
+        List of directories to be considered in the path. Every directory should contain images of a single class.
+    - labels: [int]
+        Class labels. Should correspond to classes.
+    - image_shape: tuple (H,W,C)
+        H - image height
+        W - image width
+        C - number of channels
+    - loader: Function
+        Should take path to image file and image_shape as parameters.
+        Should return numpy array
+    - num_per_class: int
+        Number of images that should be randomly chosen from each class.
+    - batch_size: int
+        Number of classes to use in a single batch. Total number of samples will be batch_size * num_per_class.
+        If batch_size == None, samples will be taken from every one of dirs.
+    - random: bool
+        If classes for a batch should be chosen randomly.
+    - start_idx: int
+        Start index, if batch is not random.
+    - end_idx: int
+        End index, if batch is not random.
+      
+    Returns:
+    --------
+    - samples: ndarray (N, H, W, C)
+        Numpy array of randomly chosen images resized according to model's input shape.
+        N - number of samples
+        H - height
+        W - width
+        C - number of channels
+    - batch_labels: [int]
+        Sample labels.
+    """
+
+    # Either random = True or batch_size is specified or start_idx and end_idx are specified
+    assert ((random == False) & (batch_size != None) & (start_idx != None) & (end_idx != None)) | (random == True) | (batch_size == None)
+
+    if batch_size == None:
+        batch_dirs = dirs
+    elif random == True:
+        batch_dirs = np.random.choice(dirs, batch_size)
+    else:
+        batch_dirs = dirs[start_idx : end_idx]
+
+    samples = np.zeros((num_per_class * len(batch_dirs), *image_shape))
+    batch_labels = np.ones(num_per_class * len(batch_dirs)).astype(int)
+        
+    for i, dir_name in enumerate(batch_dirs):
+        dir_path = os.path.join(path, dir_name)
+        filenames = os.listdir(dir_path)
+        filenames = np.random.choice(filenames, num_per_class)
+            
+        batch = np.zeros((num_per_class, *image_shape))
+
+        for j, filename in enumerate(filenames):
+            batch[j,:,:,:] = loader(os.path.join(dir_path, filename), image_shape)
+            
+        samples[i*num_per_class: i*num_per_class + num_per_class, :, :, :] = batch
+        batch_labels[i*num_per_class: i*num_per_class + num_per_class] = batch_labels[i*num_per_class: i*num_per_class + num_per_class] * labels[i]
+    
+    return samples, batch_labels
+
+def batch_of_images_generator(**kwargs):
+    """
+    Generates batches of images
+    
+    Keyword Arguments:
     -----------
     - path: string
         Path to the image source directory on disk. 
@@ -41,10 +113,6 @@ def batch_of_images_generator(path, dirs, labels, image_shape, loader, num_per_c
         Number of images that should be randomly chosen from each class.
     - batch_size: int
         Number of classes to use in a single batch. Total number of samples will be batch_size * num_per_class
-    - random: bool
-        If classes for a batch should be chosen randomly.
-    - index: int
-        Number of a current epoch
       
     Returns:
     --------
@@ -58,7 +126,8 @@ def batch_of_images_generator(path, dirs, labels, image_shape, loader, num_per_c
         Sample labels.
     """
 
-    #assert ((random == False) & (index != None)) | (random == True)
+    dirs = kwargs.get('dirs')
+    batch_size = kwargs.get('batch_size')
 
     iterations_per_epoch = math.ceil(len(dirs) / batch_size)
 
@@ -66,34 +135,13 @@ def batch_of_images_generator(path, dirs, labels, image_shape, loader, num_per_c
     while True:
         iter_count += 1
 
-        if batch_size == None:
-            batch_dirs = dirs
-        elif random == True:
-            batch_dirs = np.random.choice(dirs, batch_size)
-        else:
-            start_index = iter_count * batch_size
-            end_index = min(start_index + batch_size, len(dirs))
-            batch_dirs = dirs[start_index : end_index]
+        start_idx = iter_count * batch_size
+        end_idx = min(start_idx + batch_size, len(dirs))
 
-            if end_index >= len(dirs):
-                iter_count = -1
+        if end_idx >= len(dirs):
+            iter_count = -1
 
-        samples = np.zeros((num_per_class * len(batch_dirs), *image_shape))
-        batch_labels = np.ones(num_per_class * len(batch_dirs)).astype(int)
-        
-        for i, dir_name in enumerate(batch_dirs):
-            dir_path = os.path.join(path, dir_name)
-            filenames = os.listdir(dir_path)
-            filenames = np.random.choice(filenames, num_per_class)
-            
-            batch = np.zeros((num_per_class, *image_shape))
-
-            for j, filename in enumerate(filenames):
-                batch[j,:,:,:] = loader(os.path.join(dir_path, filename), image_shape)
-            
-            samples[i*num_per_class: i*num_per_class + num_per_class, :, :, :] = batch
-            batch_labels[i*num_per_class: i*num_per_class + num_per_class] = batch_labels[i*num_per_class: i*num_per_class + num_per_class] * labels[i]
-        
+        samples, batch_labels = load_batch_of_images(**kwargs, random=False, start_idx=start_idx, end_idx=end_idx)
         yield iter_count, samples, batch_labels
 
 def load_batch_of_data(samples, labels, batch_size, iteration):
