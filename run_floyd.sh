a9c7a784befdfee87f8a4df6c2d81a05c2484b41
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-03-21 18:48:54 +0300

    Train from scratch on batch hard

--- run_floyd.sh
+++ run_floyd.sh
@@ -3,14 +3,13 @@ python jobs.py \
     --model_name=deep_sort_cnn \
     --source_path=../input/mars/bbox_train/ \
     --model_path=../input/models/deep_sort_cnn/freeze_model.py \
-    --checkpoint_path=../input/models/deep_sort_cnn/mars-small128.ckpt-68577 \
     --batch_size=32 \
     --epochs=101 \
     --num_per_class=12 \
-    --loss=triplet_all \
-    --metric=cosine \
+    --loss=triplet_hard \
+    --metric=eucledian_squared \
     --margin=1 \
-    --lr=1e-5 \
+    --lr=1e-3 \
     --log_every=1 \
     --save_dir=./checkpoints \
     --save_every=10 \
